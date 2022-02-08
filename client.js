9cf98474b7515b0917af56b2604852786f53cdaa
BENcao318 <labber@DESKTOP-U16DFJJ.localdomain>
2022-02-07 19:28:49 -0500

    test out the connect handler

--- client.js
+++ client.js
@@ -3,15 +3,45 @@ const net = require("net");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: '172.31.197.153',
+    host: '172.22.186.161',
     port: '50541'
   });
+
+
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
   conn.on("data", (e) => {
-    // code that does something when the connection is first established
     console.log(e);
+    // console.log('Successfully connected to game server');
+  });
+
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log('Successfully connected to game server');
+    conn.write('Name: BYC');
+  });
+
+  conn.on("connect", () => {
+    setInterval(() => {
+      conn.write('Move: up');
+      setTimeout(() => {
+        conn.write('Move: left');
+      }, 300)
+    }, 600)
+    // setTimeout(() => {
+    //   conn.write('Move: up');
+    // }, 50);
+    // setTimeout(() => {
+    //   conn.write('Move: left');
+    // }, 50);
+    // setTimeout(() => {
+    //   conn.write('Move: up');
+    // }, 50);
+    // setTimeout(() => {
+    //   conn.write('Move: up');
+    // }, 50);
+
   });
 
   return conn;
